dc3a51935dc6c6daf41d42a849d735a17c559596
Angel Contreras <angelcbenoit@gmail.com>
2018-08-04 14:26:23 -0700

    get calendar displaying correctly in the table

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 
 class LandingPage extends Component {
 
-    fillCalendar(dateObject){
+    displayCalendar(datesArray) {
+        const renderedCalendar = [[], [], [], [], [], []];
+        let calendarIndex = 0;
+        for (let i = 0; i < datesArray.length; i++) {
+            if ((i + 1) % 7 === 0) {
+                renderedCalendar[calendarIndex].push(datesArray[i]);
+                calendarIndex++;
+            }
+            else {
+                renderedCalendar[calendarIndex].push(datesArray[i])
+            }
+        }
+
+        const data = (
+            <table style={{ 'width': '100%' }} cellspacing="0">
+                <tr>
+                    <th className="week-day-name">Sun</th>
+                    <th className="week-day-name">Mon</th>
+                    <th className="week-day-name">Tue</th>
+                    <th className="week-day-name">Wed</th>
+                    <th className="week-day-name">Thu</th>
+                    <th className="week-day-name">Fri</th>
+                    <th className="week-day-name">Sat</th>
+                </tr>
+
+                <tr className="week-row">{renderedCalendar[0]}</tr>
+                <tr className="week-row">{renderedCalendar[1]}</tr>
+                <tr className="week-row">{renderedCalendar[2]}</tr>
+                <tr className="week-row">{renderedCalendar[3]}</tr>
+                <tr className="week-row">{renderedCalendar[4]}</tr>
+                <tr className="week-row">{renderedCalendar[5]}</tr>
+            </table>
+        )
+
+        return data;
+    }
+
+    fillCalendar(dateObject) {
         let dates = [];
         let display = [];
-        for(let i = 0; i < dateObject.length; i++){
+        for (let i = 0; i < dateObject.length; i++) {
             let splitDateObject = dateObject[i].toString().split(" ");
             let dayOfWeek = splitDateObject[0];
             let dayNumber = splitDateObject[2];
@@ -15,51 +52,66 @@ class LandingPage extends Component {
             })
         }
 
+        for (let index = 0; index < 42; index++) {
+            display.push(<td className="week-day"></td>)
+        }
+
         //firstDayOfMonth gets the day of the week, for example monday = 1, thursday = 3, sunday = 0
         const firstDayOfMonth = this.getMonthStart(dates[0].dayWeek);
 
-        let startEmpty;
-        //startEmpty fills the first empty spots in the calendar, for example
-        //if the month begins on a tuesday, then sunday, and monday will be filled in blank
-
-        //if it's equal to 0, then that means the first week is blank because a calendar has a
-        //default of 35 spots, 5 rows with 7 days each
-        if(firstDayOfMonth === 0)
-          startEmpty = 7;
-        //otherwise keep it at whatever the first day is
-        else
-          startEmpty = firstDayOfMonth;
-
-        //first for loop fills in the first set of empty boxes in the calendar if necessary
-        for(let j = 0; j < firstDayOfMonth; j++){
-            display.push("EMPTY");
-        }
-
         //the second for loop fills in the boxes with actual dates, it will be filled in
         //with the day of the week and month number
-        for(let k = 0; k < Number(dates[dates.length-1].dayNum); k++){
-            display.push(dates[k])
+        for (let k = 0; k < Number(dates[dates.length - 1].dayNum); k++) {
+            display[k + firstDayOfMonth] = <td className="week-day">{dates[k].dayNum}</td>;
         }
 
-        //lastly, we have a while loop to keep filling the array so the calendar
-        //length is divisible by 7 because every row has 7 days in it
-        while(display.length%7 !== 0){
-            display.push("Empty")
-        }
+        return this.displayCalendar(display);
     }
 
     getDaysInMonth(month, year) {
         var date = new Date(year, month, 1);
         var days = [];
         while (date.getMonth() === month) {
-           days.push(new Date(date));
-           date.setDate(date.getDate() + 1);
+            days.push(new Date(date));
+            date.setDate(date.getDate() + 1);
         }
         return days;
-   }
+    }
 
-   getMonthStart(day){
-        switch(day){
+    getMonthName() {
+        const dateObj = new Date();
+        const monthNum = dateObj.getMonth();
+
+        switch (monthNum) {
+            case 0:
+                return "January";
+            case 1:
+                return "February";
+            case 2:
+                return "March";
+            case 3:
+                return "April";
+            case 4:
+                return "May";
+            case 5:
+                return "June";
+            case 6:
+                return "July";
+            case 7:
+                return "August";
+            case 8:
+                return "September";
+            case 9:
+                return "October";
+            case 10:
+                return "November";
+            case 11:
+                return "December";
+        }
+    }
+
+    getMonthStart(day) {
+        switch (day) {
             case "Sun":
                 return 0;
             case "Mon":
@@ -75,31 +127,19 @@ class LandingPage extends Component {
             case "Sat":
                 return 6;
         }
-   }
+    }
 
     render() {
+        const test = this.getDaysInMonth(7, 2018);
+        const cal = this.fillCalendar(test);
+        const month = this.getMonthName();
         return (
             <div className="calendar">
                 <div className="calendar_header">
-                    Month
+                    {month}
                 </div>
                 <div className="calendar_body">
-                    <div className="calendar_body__dayTitle">
-                        <ul>
-                            <li className="calendar_body__day">Sun</li>
-                            <li className="calendar_body__day">Mon</li>
-                            <li className="calendar_body__day">Tue</li>
-                            <li className="calendar_body__day">Wed</li>
-                            <li className="calendar_body__day">Thu</li>
-                            <li className="calendar_body__day">Fri</li>
-                            <li className="calendar_body__day">Sat</li>
-                        </ul>
-                    </div>
-                    <div className="calendar_body__days">
-                        <ul>
-
-                        </ul>
-                    </div>
+                    {cal}
                 </div>
             </div>
         );
