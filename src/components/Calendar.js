f4189aea7346e8b72f328db4fd0f0d4c3e67c015
Angel Contreras <angelcbenoit@gmail.com>
2018-08-04 15:48:41 -0700

    user can now change month/year by clicking the arrows. single arrow for month and double arrows for year

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 
 class LandingPage extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            month: 7,
+            year: 2018
+        }
+
+        this.changeDate = this.changeDate.bind(this);
+    }
+
+    changeDate(format ,direction){
+        let directionNum;
+
+        if(direction === "forward")
+            directionNum = 1;
+        else
+            directionNum = -1;
+
+        if(format === "month")
+            this.setState((prevState) => {
+                return {month: prevState[format] + directionNum }
+            });
+        else
+            this.setState((prevState) => {
+                return {year: prevState[format] + directionNum }
+            });
+    }
+
+
 
     displayCalendar(datesArray) {
         const renderedCalendar = [[], [], [], [], [], []];
@@ -17,14 +47,14 @@ class LandingPage extends Component {
 
         const data = (
             <table style={{ 'width': '100%' }} cellspacing="0">
-                <tr>
-                    <th className="week-day-name">Sun</th>
-                    <th className="week-day-name">Mon</th>
-                    <th className="week-day-name">Tue</th>
-                    <th className="week-day-name">Wed</th>
-                    <th className="week-day-name">Thu</th>
-                    <th className="week-day-name">Fri</th>
-                    <th className="week-day-name">Sat</th>
+                <tr className="week-name__row">
+                    <th className="week-name">Sun</th>
+                    <th className="week-name">Mon</th>
+                    <th className="week-name">Tue</th>
+                    <th className="week-name">Wed</th>
+                    <th className="week-name">Thu</th>
+                    <th className="week-name">Fri</th>
+                    <th className="week-name">Sat</th>
                 </tr>
 
                 <tr className="week-row">{renderedCalendar[0]}</tr>
@@ -53,7 +83,7 @@ class LandingPage extends Component {
         }
 
         for (let index = 0; index < 42; index++) {
-            display.push(<td className="week-day"></td>)
+            display.push(<td className="week-blank"></td>)
         }
 
         //firstDayOfMonth gets the day of the week, for example monday = 1, thursday = 3, sunday = 0
@@ -78,10 +108,7 @@ class LandingPage extends Component {
         return days;
     }
 
-    getMonthName() {
-        const dateObj = new Date();
-        const monthNum = dateObj.getMonth();
-
+    getMonthName(monthNum) {
         switch (monthNum) {
             case 0:
                 return "January";
@@ -130,14 +157,20 @@ class LandingPage extends Component {
     }
 
     render() {
-        const test = this.getDaysInMonth(7, 2018);
+        const test = this.getDaysInMonth(this.state.month, this.state.year);
         const cal = this.fillCalendar(test);
-        const month = this.getMonthName();
+        const month = this.getMonthName(this.state.month);
         return (
             <div className="calendar">
                 <div className="calendar_header">
-                    {month}
+                    <i className="fa fa-angle-double-left arrows" onClick={() => this.changeDate("year", "backward")}></i>
+                    <i className="fa fa-angle-left arrows" onClick={() => this.changeDate("month", "backward")}></i>
+                    {month} {this.state.year}
+                    <i className="fa fa-angle-right arrows" onClick={() => this.changeDate("month", "forward")}></i>
+                    <i className="fa fa-angle-double-right arrows" onClick={() => this.changeDate("year", "forward")}></i>
+
                 </div>
+
                 <div className="calendar_body">
                     {cal}
                 </div>
